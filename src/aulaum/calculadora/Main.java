16d756c4641942d716b2b75135c56297a8cd2eb4
Josiane Carneiro <josiane.carneiro04@gmail.com>
2022-04-17 09:14:47 -0300

    Aula Metodos em Java Exec1

---
+++ src/aulaum/calculadora/Main.java
@@ -0,0 +1,30 @@
+package aulaum.calculadora;
+
+public class Main {
+    public static void main(String[] args) {
+        //Calculadores
+        System.out.println("Exercicio calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 4);
+        Calculadora.multiplicacao(7, 8);
+        Calculadora.divisao(5, 2.5);
+
+        //Mensagem
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem( 14);
+        Mensagem.obterMensagem(1);
+
+        //Emprestimo
+        System.out.println("Exercicio emprestimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+
+
+
+
+
+    }
+
+}
